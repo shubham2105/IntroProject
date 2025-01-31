1b35b7c649212ae5de7a714051891c948ae08908
Shubham Dhole <shubhamdhole215@gmail.com>
2025-01-31 23:35:56 +0530

    added basic email id check for signup

--- screens/SignUpScreen.jsx
+++ screens/SignUpScreen.jsx
@@ -24,8 +24,16 @@ const HomeScreen = ({navigation}) => {
       .then(() => {
         Alert.alert('User Signed Up Successfully');
       })
-      .catch(err => {
-        console.error(err);
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+          Alert.alert('That email address is already in use!');
+        }
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+          Alert.alert('That email address is invalid!');
+        }
+        console.error(error);
       });
   };
 
