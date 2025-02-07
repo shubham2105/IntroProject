99352c014154ef2e6af093a32c99f16a8234a265
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-07 22:43:50 +0530

    "Reverted changes for build issues"
    This reverts commit 61cd0a966249847fbbb87f61a3471b200fc9a843.

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -20,17 +20,16 @@ const HomeScreen = ({route}) => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.greetText}>Welcome {userName} </Text>
-      <View style={styles.textInputContainer}>
+      <View>
         <TextInput
-          style={styles.textInput}
           placeholder="Enter todo"
           value={todo}
           onChangeText={setTodo}
           autoCorrect={false}
         />
         <View>
-          <Pressable style={styles.addtodobtn} onPress={() => {}}>
-            <Text style={styles.buttontxt}>Add Todo</Text>
+          <Pressable onPress={() => {}}>
+            <Text>Add Todo</Text>
           </Pressable>
         </View>
       </View>
@@ -50,36 +49,5 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     padding: '5%',
-  },
-  addtodobtn: {
-    backgroundColor: 'blue',
-    width: '40%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '2%',
-    borderRadius: 5,
-    marginBottom: '4%',
-  },
-  textInputContainer: {
-    width: '80%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '2%',
-  },
-  textInput: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: '5%',
-    padding: '2%',
-  },
-  buttontxt: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: 'white',
   },
 });
